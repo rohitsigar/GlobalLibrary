ba2d6b79d5a7208db07b2cccf54243f69a3b665c
manikmmalhotra <manikmalhotra2113@gmail.com>
2020-12-17 00:29:22 +0530

    init

---
+++ app/src/main/java/com/example/globallibrary/Activity/AuthenticationActivity.java
@@ -0,0 +1,16 @@
+package com.example.globallibrary.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.globallibrary.R;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
