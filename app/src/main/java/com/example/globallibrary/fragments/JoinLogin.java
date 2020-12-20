2d701c0fb1230f47d91221b71212bdb54ee79fbe
manikmmalhotra <manikmalhotra2113@gmail.com>
2020-12-20 19:27:27 +0530

    Join n login

---
+++ app/src/main/java/com/example/globallibrary/fragments/JoinLogin.java
@@ -0,0 +1,40 @@
+package com.example.globallibrary.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.example.globallibrary.Adpaters.JoinLoginAdapter;
+import com.example.globallibrary.R;
+
+public class JoinLogin extends Fragment {
+
+    RecyclerView recyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_join_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = view.findViewById(R.id.branchList);
+        JoinLoginAdapter joinLoginAdapter = new JoinLoginAdapter(getContext());
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(joinLoginAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL,false));
+    }
+}
