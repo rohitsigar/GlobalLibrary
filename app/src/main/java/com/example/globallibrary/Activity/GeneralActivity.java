85c5229f78e5bafee23c3dbc4a2e35edef7c11cc
rohitsigar <rohitsigar05@gmail.com>
2020-12-22 22:46:16 +0530

    init

--- app/src/main/java/com/example/globallibrary/Activity/GeneralActivity.java
+++ app/src/main/java/com/example/globallibrary/Activity/GeneralActivity.java
@@ -1,5 +1,6 @@
 package com.example.globallibrary.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,38 +19,46 @@ import android.widget.Toolbar;
 import com.example.globallibrary.R;
 import com.google.android.material.navigation.NavigationView;
 
-public class GeneralActivity extends AppCompatActivity {
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
-    androidx.appcompat.widget.Toolbar toolbar;
-    Menu menu;
+public class GeneralActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+  DrawerLayout drawerLayout;
+   NavigationView navigationView;
+   androidx.appcompat.widget.Toolbar toolbar;
+   Menu menu;
     TextView textView;
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general);
-         drawerLayout = findViewById(R.id.drawerlayout);
-        navigationView = findViewById(R.id.slider);
-        toolbar=findViewById(R.id.toolbar);
-         setSupportActionBar(toolbar);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open , R.string.close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
+      drawerLayout = findViewById(R.id.drawer_layout);
+     navigationView = findViewById(R.id.slider);
+      navigationView.bringToFront();
+      toolbar=findViewById(R.id.toolbar);
+      setSupportActionBar(toolbar);
+     ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open , R.string.close);
+   drawerLayout.addDrawerListener(toggle);
+   toggle.syncState();
+       navigationView.setNavigationItemSelectedListener(this);
     }
+
+   @Override
+   public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+       {
+          drawerLayout.closeDrawer(GravityCompat.START);
+      }
+       else
+       {
+          super.onBackPressed();
+       }
+   }
+
+
+
 
     @Override
-    public void onBackPressed() {
-        if(drawerLayout.isDrawerOpen(GravityCompat.START))
-        {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        }
-        else
-        {
-            super.onBackPressed();
-        }
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        return true;
     }
-
-
-
 }
