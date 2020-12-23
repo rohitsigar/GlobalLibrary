c610bb85f1979baf30384d685cfca5d33f6fd150
manikmmalhotra <manikmalhotra2113@gmail.com>
2020-12-23 23:31:49 +0530

    home fragment

--- app/src/main/java/com/example/globallibrary/Fragment/HomeStudentFragment.java
+++ app/src/main/java/com/example/globallibrary/Fragment/HomeStudentFragment.java
@@ -5,16 +5,19 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.globallibrary.Adpaters.JoinLoginAdapter;
 import com.example.globallibrary.R;
 
 public class HomeStudentFragment extends Fragment {
 
-
+    RecyclerView recyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,5 +30,10 @@ public class HomeStudentFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        recyclerView = view.findViewById(R.id.homeRecycler);
+        JoinLoginAdapter joinLoginAdapter = new JoinLoginAdapter(getContext());
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(joinLoginAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL,false));
     }
 }
